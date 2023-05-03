9c5ae11b33a25cebfe7c77a92805b4c30d9ff791
Samuel Massnick <smassnick24@adrian.edu>
2023-05-03 13:53:54 -0400

    pdf reader works, pdf writer doesnt, likeyly design oversight

--- tests.py
+++ tests.py
@@ -1,23 +1,10 @@
 from PyPDF2 import PdfReader, PdfWriter
 
-with open("Character-Sheet.pdf", "rb") as pdf:
-    page_container = []
-
-    reader = PdfReader(pdf)
-    fields = reader.get_form_text_fields()
-
-    # print(fields)
-
-    for pages in reader.pages:
-        page_container.append(pages.extract_text())
-
-        # print(page_container)
-
 
 def fill_form():
     char_name = input("What is your Character's Name?: ")
     race = input("What is your Character's Race?: ")
-    alignment = input("What is your Character's Race?: ")
+    alignment = input("What is your Character's Alignment?: ")
     initiative = input("Initiative: ")
     max_hp = input("Maximum HP: ")
     stats = [input("Strength: "), input("Dexterity: "), input("Constitution: "),
@@ -37,8 +24,52 @@ def fill_form():
             elif stat > 10:
                 storage = stat - 10
                 modifier = storage // 2
-                stat_mods.append(modifier)
+                stat_mods.append("+" + str(modifier))
             else:
                 storage = 10 - stat
                 modifier = (storage // 2) * -1
-                stat_mods.append(modifier)
+                stat_mods.append("-" + str(modifier))
+
+    filled_fields = {
+        "CharacterName": char_name,
+        "Race ": race,
+        "Alignment": alignment,
+        "Initiative": int(initiative),
+        "HPMax": int(max_hp),
+        "STR": stats[0],
+        "STRmod": stat_mods[0],
+        "DEX": stats[1],
+        "DEXmod ": stat_mods[1],
+        "CON": stats[2],
+        "CONmod": stat_mods[2],
+        "INT": stats[3],
+        "INTmod": stat_mods[3],
+        "WIS": stats[4],
+        "WISmod": stat_mods[4],
+        "CHA": stats[5],
+        "CHamod": stat_mods[5]
+    }
+
+    with open("Character-Sheet.pdf", "rb") as pdf:
+        page_container = []
+
+        reader = PdfReader(pdf)
+        writer = PdfWriter()
+        fields = reader.get_fields()
+
+        page_one = reader.pages[0]
+        writer.add_page(page_one)
+
+        # print(fields)
+
+        for pages in reader.pages:
+            page_container.append(pages.extract_text())
+
+    fields.update(filled_fields)
+    writer.update_page_form_field_values(writer.pages[0], fields)
+
+    with open("Character-Sheet.pdf", "wb") as output:
+        writer.write(output)
+
+
+fill_form()
