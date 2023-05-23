e87a34f12e9495f24b1a777bc3fd08913473452a
Samuel Massnick <smassnick24@adrian.edu>
2023-05-23 16:02:20 -0400

    refactoring

--- CreateCharacterClass.py
+++ CreateCharacterClass.py
@@ -6,4 +6,7 @@ class CharacterCreator(object):
     def __init__(self):
         self.template = "template/DnD_5E_CharacterSheet_FormFillable.pdf"
         self.output = "character_sheets/Default.pdf"
+        self.fields = {
+
+        }
 
